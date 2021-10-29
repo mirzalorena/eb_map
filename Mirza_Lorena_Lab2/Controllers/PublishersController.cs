133aee83ac328c143f0dbf52ffb783693a03ca04
mirzalorena <mlie2568@scs.ubbcluj.ro>
2021-10-29 17:25:48 +0300

    publishers initial done

---
+++ Mirza_Lorena_Lab2/Controllers/PublishersController.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Mirza_Lorena_Lab2.Data;
+using Mirza_Lorena_Lab2.Models;
+
+using Mirza_Lorena_Lab2.Models.LibraryViewModels;
+
+namespace Mirza_Lorena_Lab2.Controllers
+{
+    public class PublishersController : Controller
+    {
+        private readonly LibraryContext _context;
+
+        public PublishersController(LibraryContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Publishers
+        public async Task<IActionResult> Index(int? id, int? bookID)
+        {
+            var viewModel = new PublisherIndexData();
+            viewModel.Publishers = await _context.Publishers
+            .Include(i => i.PublishedBooks)
+            .ThenInclude(i => i.Book)
+            .ThenInclude(i => i.Orders)
+            .ThenInclude(i => i.Customer)
+            .AsNoTracking()
+            .OrderBy(i => i.PublisherName)
+            .ToListAsync();
+            if (id != null)
+            {
+                ViewData["PublisherID"] = id.Value;
+                Publisher publisher = viewModel.Publishers.Where(
+                i => i.ID == id.Value).Single();
+                viewModel.Books = publisher.PublishedBooks.Select(s => s.Book);
+            }
+            if (bookID != null)
+            {
+                ViewData["BoookID"] = bookID.Value;
+                viewModel.Orders = viewModel.Books.Where(
+                x => x.ID == bookID).Single().Orders;
+            }
+            return View(viewModel);
+        }
+ 
+        // GET: Publishers/Details/5
+            public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var publisher = await _context.Publishers
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (publisher == null)
+            {
+                return NotFound();
+            }
+
+            return View(publisher);
+        }
+
+        // GET: Publishers/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Publishers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,PublisherName,Adress")] Publisher publisher)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(publisher);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(publisher);
+        }
+
+        // GET: Publishers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var publisher = await _context.Publishers.FindAsync(id);
+            if (publisher == null)
+            {
+                return NotFound();
+            }
+            return View(publisher);
+        }
+
+        // POST: Publishers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,PublisherName,Adress")] Publisher publisher)
+        {
+            if (id != publisher.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(publisher);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PublisherExists(publisher.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(publisher);
+        }
+
+        // GET: Publishers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var publisher = await _context.Publishers
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (publisher == null)
+            {
+                return NotFound();
+            }
+
+            return View(publisher);
+        }
+
+        // POST: Publishers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var publisher = await _context.Publishers.FindAsync(id);
+            _context.Publishers.Remove(publisher);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PublisherExists(int id)
+        {
+            return _context.Publishers.Any(e => e.ID == id);
+        }
+    }
+}
